a1d02a74c0fb296010f4b92d996ad39dc53f8941
Ali Hamza <alihamza.mentorsol@gmail.com>
2023-07-25 18:36:34 +0500

    Added Landing Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,34 @@
 <template>
-  <div class="Home-banner">
-    <div class="container inner-container">
-      <div class="inner-banner">
-        <img :src="ShareIcon" alt="Share" />
-        <div class="banner-desc">
-          <p>Risk is the price you pay for opportunity.</p>
+  <template>
+    <div class="Home-banner">
+      <div class="container inner-container">
+        <div class="inner-banner">
+          <img :src="ShareIcon" alt="Share" />
+          <div class="banner-desc">
+            <p>Risk is the price you pay for opportunity.</p>
+          </div>
         </div>
-      </div>
-      <div class="row m-0 p-0">
-        <div class="col-lg-12 m-0 p-0">
-          <div class="d-flex justify-content-between inspired-section align-items-center my-2">
-            <p style="font-family: Roboto;
+        <div class="row m-0 p-0">
+          <div class="col-lg-12 m-0 p-0">
+            <div class="d-flex justify-content-between inspired-section align-items-center my-2">
+              <p style="font-family: Roboto;
 font-size: 18px;
 font-weight: 300;
 line-height: 28px;
 letter-spacing: 0px;
 text-align: left;
 ">Get Inspired</p>
-            <a href="#">Explore More</a>
+              <a href="#">Explore More</a>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="row inner-cards m-0 p-0">
-        <InsipiredCard />
-      </div>
-      <div class="row p-0 m-0">
-        <div class="col-lg-12 p-0 m-0">
-          <div class="d-flex justify-content-between inspired-section bizlyupdate align-items-baseline  my-2">
-            <p style="
+        <div class="row inner-cards m-0 p-0">
+          <InsipiredCard />
+        </div>
+        <div class="row p-0 m-0">
+          <div class="col-lg-12 p-0 m-0">
+            <div class="d-flex justify-content-between inspired-section bizlyupdate align-items-baseline  my-2">
+              <p style="
                 font-family: Ubuntu;
                 font-size: 1.125rem;
                 font-style: italic;
@@ -36,30 +37,30 @@ text-align: left;
                 letter-spacing: 0px;
                 text-align: left;
               ">
-              bizUpdates
-            </p>
-            <a href="#">See All</a>
+                bizUpdates
+              </p>
+              <a href="#">See All</a>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="row m-0 p-0 updates-section">
-        <div class="col-lg-6 m-0 p-0 update-card">
-          <BizlyupdateCard />
+        <div class="row m-0 p-0 updates-section">
+          <div class="col-lg-6 m-0 p-0 update-card">
+            <BizlyupdateCard />
+          </div>
+          <div class="col-lg-6 m-0 p-0 update-card">
+            <BizlyupdateCard />
+          </div>
+          <div class="col-lg-6 m-0 p-0 update-card">
+            <BizlyupdateCard />
+          </div>
+          <div class="col-lg-6 m-0 p-0 update-card">
+            <BizlyupdateCard />
+          </div>
         </div>
-        <div class="col-lg-6 m-0 p-0 update-card">
-          <BizlyupdateCard />
-        </div>
-        <div class="col-lg-6 m-0 p-0 update-card">
-          <BizlyupdateCard />
-        </div>
-        <div class="col-lg-6 m-0 p-0 update-card">
-          <BizlyupdateCard />
-        </div>
-      </div>
-      <div class="row m-0 p-0">
-        <div class="col-lg-12 m-0 p-0">
-          <div class="inspired-section">
-            <p style="
+        <div class="row m-0 p-0">
+          <div class="col-lg-12 m-0 p-0">
+            <div class="inspired-section">
+              <p style="
                 font-family: Roboto;
                 font-size: 18px;
                 font-weight: 300;
@@ -67,36 +68,37 @@ text-align: left;
                 letter-spacing: 0px;
                 text-align: left;
               ">
-              Get in touch
-            </p>
-          </div>
-          <div>
-            <GetinTouch />
+                Get in touch
+              </p>
+            </div>
+            <div>
+              <GetinTouch />
+            </div>
           </div>
         </div>
-      </div>
-      <div class="row m-0 p-0">
-        <HomeTabs />
+        <div class="row m-0 p-0">
+          <HomeTabs />
+        </div>
       </div>
     </div>
-  </div>
-  <div class="tabs-main">
-    <div class="container p-3 p-md-0 ">
-      <div class="tab-content" id="myTabContentHome">
-        <div class="tab-pane fade show active" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-          <div className="TabsWrapper">
-            <PostView />
+    <div class="tabs-main">
+      <div class="container p-3 p-md-0 ">
+        <div class="tab-content" id="myTabContentHome">
+          <div class="tab-pane fade show active" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+            <div className="TabsWrapper">
+              <PostView />
+            </div>
           </div>
-        </div>
-        <!-- <div class="tab-pane fade" id="content" role="tabpanel" aria-labelledby="content-tab">
+          <!-- <div class="tab-pane fade" id="content" role="tabpanel" aria-labelledby="content-tab">
           <h2>hi</h2>
         </div>
         <div class="tab-pane fade" id="services" role="tabpanel" aria-labelledby="services-tab">
           <h1>heloooo</h1>
         </div> -->
+        </div>
       </div>
     </div>
-  </div>
+  </template>
 </template>
 <script setup>
 import BizlyupdateCard from "../components/Home/BizlyupdateCard.vue";
@@ -104,16 +106,11 @@ import GetinTouch from "../components/Home/GetinTouch.vue";
 import HomeTabs from "../components/Home/HomeTabs.vue";
 import InsipiredCard from "../components/Home/InsipiredCard.vue";
 import ShareIcon from "~/src/assets/images/share2.svg";
-// import TabSevnthSection from "../components/TabSevnthSection.vue";
-// import TabsEightSection from "../components/TabsEightSection.vue";
-// import TabsNightSection from "../components/TabsNightSection.vue";
-// import TabsSixthSection from "../components/TabsSixthSection.vue";
-// import TabstenthSection from "../components/TabstenthSection.vue";
-// import TabsFirstSection from "../components/tabsFirstSection.vue";
-// import TabsSecondSection from "../components/tabsSecondSection.vue";
-// import TabsVideoSection from "../components/tabsVideoSection.vue";
-// import TabsfifthSection from "../components/tabsfifthSection.vue";
 import PostView from "../components/PostView.vue";
+import LandingPageNew from "../components/LandingPageNew/LandingPageNew.vue";
+import { storeToRefs } from 'pinia';
+import { useStore } from "../store";
+const { isAuthenticated } = storeToRefs(useStore());
 </script>
 <style scoped lang="scss">
 .inspired-section {
